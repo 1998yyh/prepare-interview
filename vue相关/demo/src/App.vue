0fad92538ef7d7066d1a89cc6b7c81adae4b7b38
yanghao <532438158@qq.com>
2021-04-06 23:10:44 +0800

    【新增】vuex

---
+++ vue相关/demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  mounted(){
+    console.log(this.$store.state);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue相关/demo/src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex';
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+      // 定义一个name，以供全局使用
+      name: '张三',
+      // 定义一个number，以供全局使用
+      number: 0,
+      // 定义一个list，以供全局使用
+      list: [
+          { id: 1, name: '111' },
+          { id: 2, name: '222' },
+          { id: 3, name: '333' },
+      ]
+  },
+});
+
+export default store;
