c09a8a610a4c24e5372b51b463d553dfcfa71409
1998yyh <41160134+1998yyh@users.noreply.github.com>
2021-05-27 07:28:57 +0800

    【新增功能】可组合的vue3

---
+++ vue相关/vue3/my-vue-app/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style>
+
+</style>
---
+++ vue相关/vue3/my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>ref自动解包</p>
+</template>
+
+<script setup>
+import { ref , reactive , watch} from 'vue';
+
+const counter = ref(0);
+
+
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
